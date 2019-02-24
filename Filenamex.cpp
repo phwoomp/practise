b9e2e5dc44cdc3bf440ca12b2005a7a0f2895ce5
phwoomp <44414896+phwoomp@users.noreply.github.com>
2019-02-24 22:13:53 +0100

    Add files via upload

---
+++ Filenamex.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+ //Compiler version g++ 6.3.0
+
+ int main()
+ {
+ 	std::string userInput;
+ 	std::cout << "type something: ";
+ 	std::getline(std::cin, userInput);
+ 	std::cout << userInput;
+ 	return 0; 
+ }
+ 
+
+    
